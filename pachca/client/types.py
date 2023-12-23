48fb88b52110c56b04a5a81a6bf978b16f650639
NV_kovalev <anit200@yandex.ru>
2023-12-23 23:33:50 +0300

    Доработан класс RequestData

--- pachca/client/types.py
+++ pachca/client/types.py
@@ -1,28 +1,25 @@
-from typing import Any, Dict, Optional
-
 from pydantic import BaseModel
+
+
+class ChatData(BaseModel):
+
+    name: str
+    member_ids: tuple[int] = None
+    group_tag_ids: tuple[int] = None
+    channel: bool = None
+    public: bool = None
+
+
+class RequestData(BaseModel):
+
+    chat: ChatData = None
+
+    def to_dict(self):
+        return self.model_dump(exclude_none=True)
 
 
 class Request(BaseModel):
     http_method: str = None
     url: str = None
     acceptable_statuses: tuple[int] = None
-    data: Dict[str, Optional[Any]] = None
-
-
-class RequestData(BaseModel):
-
-    name: str = None
-    member_ids: tuple[int] = None
-    group_tag_ids: tuple[int] = None
-    channel: bool = None
-    public: bool = None
-
-    def __init__(self, data: dict = None):
-        super().__init__()
-        if data is not None:
-            for key, value in data.items():
-                setattr(self, key, value)
-
-    def to_dict(self):
-        return self.__dict__
+    data: RequestData = None
