7a6bef05dabfa3a9bb527127b9a7386495d0102b
Sobiyk <kirill.sobkovskii@yandex.ru>
2023-12-24 04:22:53 +0300

    Добавил распаковку словаря в вызов функции для её корректной работы

--- pachca/methods/tasks.py
+++ pachca/methods/tasks.py
@@ -7,5 +7,5 @@ class TasksMethods:
     @classmethod
     async def create_task(cls, client: HttpClient, data: dict):
         request: Request = Router.create_task()
-        request.data = RequestData(data).to_dict()
+        request.data = RequestData(**data).to_dict()
         return await client.make_request(request)
