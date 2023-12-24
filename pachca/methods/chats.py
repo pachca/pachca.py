9a8fa74fbb394d0e97572df710be702d4a8b1f9e
Матвей Слуцких <poustplay@yandex.ru>
2023-12-24 09:51:04 +0300

    fix

--- pachca/methods/chats.py
+++ pachca/methods/chats.py
@@ -10,14 +10,13 @@ class ChatsMethods:
         return await client.make_request(request)
 
     @classmethod
-    async def get_chat_by_id(cls, client: HttpClient, id: int):
-        request: Request = Router.get_chat_by_id(id)
-        return await client.make_request(request)
+    async def get_chat_by_id(cls, *args, **kwargs):
+        pass
 
     @classmethod
     async def create_chat(cls, client: HttpClient, data: dict):
         request: Request = Router.create_chat()
-        request.data = {'chat': RequestData(data).to_dict()}
+        request.data = RequestData(**data).to_dict()
         return await client.make_request(request)
 
     @classmethod
@@ -27,7 +26,5 @@ class ChatsMethods:
         return await client.make_request(request)
 
     @classmethod
-    async def add_tags_to_chat(cls, client: HttpClient, data: dict):
-        request: Request = Router.add_tags_to_chat()
-        request.data = RequestData(data).to_dict()
-        return await client.make_request(request)
+    async def add_tags_to_chat(cls, *args, **kwargs):
+        pass
