77befaa8343b28105237762165993bcc42618e71
Матвей Слуцких <poustplay@yandex.ru>
2023-12-25 22:53:59 +0300

    Добавлены методы в bot.py и докстринг к ним

--- pachca/bot.py
+++ pachca/bot.py
@@ -26,6 +26,22 @@ class Bot:
         """
         return await BotMethods.get_user_by_id(self.client, id=id)
 
+    async def get_chats(self):
+        """
+        Метод для получение списка бесед и каналов.
+        """
+        return await BotMethods.get_chats(self.client)
+
+    async def get_chat_by_id(self, id: int = None):
+        """
+        Метод для получение информации о беседе или канале.
+        Необходимые параметры:
+
+        id: int
+
+        """
+        return await BotMethods.get_chat_by_id(self.client, id=id)
+
     async def create_chat(self, chat: dict = None):
         """
         Метод для создания новой беседы или канала.
@@ -43,3 +59,26 @@ class Bot:
 
         """
         return await BotMethods.create_chat(self.client, chat)
+
+    async def add_members_to_chat(self, member_ids: dict = None):
+        """
+        Метод для добавления пользователей в состав участников
+        беседы или канала.
+        Необходимые параметры:
+
+        member_ids: list[int] - Массив идентификаторов пользователей,
+                                    которые станут участниками
+
+        """
+        return await BotMethods.add_members_to_chat(self.client, member_ids)
+
+    async def add_tags_to_chat(self, group_tag_ids: dict = None):
+        """
+        Метод для добавления тегов в состав участников беседы или канала.
+        Необходимые параметры:
+
+        group_tag_ids: list[int] - Массив идентификаторов тегов,
+                                    которые станут участниками
+
+        """
+        return await BotMethods.add_tags_to_chat(self.client, group_tag_ids)
