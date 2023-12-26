094dda5004699aadf428adb233cf4b591b00306f
Sobiyk <kirill.sobkovskii@yandex.ru>
2023-12-26 19:05:15 +0300

    Добавил докстринги

--- pachca/bot.py
+++ pachca/bot.py
@@ -27,45 +27,20 @@ class Bot:
         return await BotMethods.get_user_by_id(self.client, id=id)
 
     async def get_group_tags(self):
+        """
+        Метод для получения актуального списка тегов сотрудников.
+        Названия тегов являются уникальными в компании.
+        """
         return await BotMethods.get_group_tags(self.client)
 
     async def get_tag_users(self, tag_id):
+        """
+        Метод для получения актуального списка сотрудников тега.
+        Необходимые параметы:
+
+        tag_id: int
+        """
         return await BotMethods.get_group_tag_users(self.client, tag_id=tag_id)
-    
-    async def get_chats(self):
-        """
-        Метод для получение списка бесед и каналов.
-        """
-        return await BotMethods.get_chats(self.client)
-
-    async def get_chat_by_id(self, id: int = None):
-        """
-        Метод для получение информации о беседе или канале.
-        Необходимые параметры:
-
-        id: int
-
-        """
-        return await BotMethods.get_chat_by_id(self.client, id=id)
-
-    async def create_chat(self, chat: dict):
-        """
-        Метод для создания новой беседы или канала.
-        Необходимые параметры:
-
-        chat {
-            name: str - Название
-            member_ids: list[int] - Массив идентификаторов пользователей,
-                                    которые станут участниками
-            group_tag_ids: list[int] - Массив идентификаторов тегов,
-                                       которые станут участниками
-            channel: bool - Тип: беседа (по умолчанию, false) или канал (true)
-            public: Доступ: закрытый (по умолчанию, false) или открытый (true)
-        }
-
-        """
-        return await BotMethods.create_chat(self.client, chat)
-
 
     async def upload_file(self, file_path: str) -> str:
         """
@@ -172,6 +147,40 @@ class Bot:
 
         """
         return await BotMethods.edit_message(self.client, id, message)
+
+    async def get_chats(self):
+        """
+        Метод для получение списка бесед и каналов.
+        """
+        return await BotMethods.get_chats(self.client)
+
+    async def get_chat_by_id(self, id: int = None):
+        """
+        Метод для получение информации о беседе или канале.
+        Необходимые параметры:
+
+        id: int
+
+        """
+        return await BotMethods.get_chat_by_id(self.client, id=id)
+
+    async def create_chat(self, chat: dict):
+        """
+        Метод для создания новой беседы или канала.
+        Необходимые параметры:
+
+        chat {
+            name: str - Название
+            member_ids: list[int] - Массив идентификаторов пользователей,
+                                    которые станут участниками
+            group_tag_ids: list[int] - Массив идентификаторов тегов,
+                                       которые станут участниками
+            channel: bool - Тип: беседа (по умолчанию, false) или канал (true)
+            public: Доступ: закрытый (по умолчанию, false) или открытый (true)
+        }
+
+        """
+        return await BotMethods.create_chat(self.client, chat)
 
     async def add_members_to_chat(self, member_ids: dict = None):
         """
