dcef530c2dcc82e90fd6d043dcdbf000e737d3fc
Никита Ковалев <atar1boy@yandex.ru>
2024-03-10 13:25:34 +0300

    Удален лишний файл

--- pachca/routers/base.py
+++
@@ -1,7 +0,0 @@
-class BaseRouter:
-
-    BASE_URL = 'https://api.pachca.com/api/shared/v1/{postfix}'
-
-    @classmethod
-    def _make_endpoint(cls, url) -> str:
-        return cls.BASE_URL.format(postfix=url)
