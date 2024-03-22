753d02c8af3d17c9db2669c2b148194cd0754c4b
Александр Жданов <canshok89@gmail.com>
2024-03-22 11:46:57 +0500

    Updated group tags methods

--- pachca/methods/group_tags.py
+++ pachca/methods/group_tags.py
@@ -5,13 +5,11 @@ from pachca.routers import Router
 class GroupTagsMethods:
 
     @classmethod
-    async def get_group_tags(cls, *args, **kwargs):
-        client: HttpClient = kwargs.get('client')
+    async def get_group_tags(cls, *args, client: HttpClient, **kwargs):
         request: Request = Router.get_group_tags()
         return await client.make_request(request)
 
     @classmethod
-    async def get_group_tag_users(cls, *args, tag_id: int, **kwargs):
-        client: HttpClient = kwargs.get('client')
+    async def get_group_tag_users(cls, *args, client: HttpClient, tag_id: int, **kwargs):
         request: Request = Router.get_group_tag_users(tag_id)
         return await client.make_request(request)
