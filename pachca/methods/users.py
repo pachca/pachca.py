8524aedd59f369d5f4b3e927eac79418161a5878
Александр Жданов <canshok89@gmail.com>
2024-03-22 11:43:31 +0500

    Updated user methods

--- pachca/methods/users.py
+++ pachca/methods/users.py
@@ -5,13 +5,11 @@ from pachca.routers import Router
 class UserMethods:
 
     @classmethod
-    async def get_users(cls, *args, **kwargs):
-        client: HttpClient = kwargs.get('client')
+    async def get_users(cls, *args, client: HttpClient, **kwargs):
         request: Request = Router.get_users()
         return await client.make_request(request)
 
     @classmethod
-    async def get_user_by_id(cls, *args, id: int, **kwargs):
-        client: HttpClient = kwargs.get('client')
+    async def get_user_by_id(cls, *args, client: HttpClient, id: int, **kwargs):
         request: Request = Router.get_user_by_id(id)
         return await client.make_request(request)
