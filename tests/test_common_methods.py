1106eac7efc9fddebede1227536e921181707a6c
greengoblinalex <greengoblinalex@mail.ru>
2024-03-26 06:53:52 +0300

    updated test_upload_file_correct_data

--- tests/test_common_methods.py
+++ tests/test_common_methods.py
@@ -28,24 +28,31 @@ class TestCommonMethods(TestBaseClient):
         """Тестирует метод 'upload_file' с корректными данными'.
 
         Проверяет корректность возвращаемых данных
-        (итоговый путь к файлу, который  будет использоваться для
-        его прикрепления к сообщению и в других методах) при
+        (итоговый файл, который  будет использоваться для
+        прикрепления к сообщению и в других методах) при
         безошибочном выполнении ботом метода 'upload_file'.
         """
         upload_file_path = os.path.abspath(__file__).replace('\\', '/')
-        upload_file_name = upload_file_path.split('/')[-1]
-        expect_response_data = (
-            EXPECT_RESPONSE_DATA_UPLOAD_FILE + upload_file_name
-        )
         self.mock.return_value = EXPECT_RESPONSE_DATA_UPLOADS
-        response = await self.bot.upload_file(upload_file_path, file_type='file')
+        response = await self.bot.upload_file(upload_file_path, file_type=EXPECT_RESPONSE_DATA_UPLOAD_FILE['file_type'])
+
         self.assertEqual(
-            response,
-            expect_response_data,
-            msg=(
-                "При безошибочном выполнении ботом метода 'upload_file' "
-                "должен возвращаться итоговый путь к файлу, который "
-                "будет использоваться для его прикрепления к сообщению "
-                "и в других методах."
-            )
+            response.key,
+            EXPECT_RESPONSE_DATA_UPLOAD_FILE['key'],
+            msg="Проверка ключа файла"
+        )
+        self.assertEqual(
+            response.name,
+            EXPECT_RESPONSE_DATA_UPLOAD_FILE['name'],
+            msg="Проверка имени файла"
+        )
+        self.assertEqual(
+            response.file_type,
+            EXPECT_RESPONSE_DATA_UPLOAD_FILE['file_type'],
+            msg="Проверка типа файла"
+        )
+        self.assertEqual(
+            response.size,
+            os.path.getsize(upload_file_path),
+            msg="Проверка размера файла"
         )
