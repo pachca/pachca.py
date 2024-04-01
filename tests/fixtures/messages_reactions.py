adf42d9245089f1337227b6d03537032dcb69357
greengoblinalex <greengoblinalex@mail.ru>
2024-04-02 00:48:48 +0300

    fixed workflow

--- tests/fixtures/messages_reactions.py
+++ tests/fixtures/messages_reactions.py
@@ -3,7 +3,6 @@ REACTION = '👍'
 REACTION_INCORRECT = 123
 
 LIST_REACTIONS = {
-<<<<<<< HEAD
     'data': [
         {
             'user_id': 76243,
@@ -41,44 +40,4 @@ LIST_REACTIONS = {
             'code': '🔥'
         }
     ]
-=======
-    'data':
-        [
-            {
-                'user_id': 76243,
-                'created_at': '2023-09-11T14:59:35.000Z',
-                'code': '👍'
-            },
-            {
-                'user_id': 10764,
-                'created_at': '2023-09-11T15:00:31.000Z',
-                'code': '👍'
-            },
-            {
-                'user_id': 27494,
-                'created_at': '2023-09-11T15:01:27.000Z',
-                'code': '👍'
-            },
-            {
-                'user_id': 27494,
-                'created_at': '2023-09-11T15:01:47.000Z',
-                'code': '🔥'
-            },
-            {
-                'user_id': 11887,
-                'created_at': '2023-09-11T15:12:49.000Z',
-                'code': '👍'
-            },
-            {
-                'user_id': 11887,
-                'created_at': '2023-09-11T15:13:46.000Z',
-                'code': '⭐'
-            },
-            {
-                'user_id': 11887,
-                'created_at': '2023-09-11T15:13:47.000Z',
-                'code': '🔥'
-            }
-        ]
->>>>>>> 7d8f9b365dc3c812b191a0a4387fe4cd0ccb69b2
 }
